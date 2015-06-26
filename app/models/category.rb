33442dd2f111818a13fd1eda3937db91e24d71ef
Clayton Perszyk <clayperszyk@gmail.com>
2015-06-25 18:50:44 -0600

    add faker gem and seed data

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ActiveRecord::Base
 	has_many :article_categories
 	has_many :articles, through: :article_categories
 
-	validates :name, presence: true
+	validates :name, presence: true,
 	                 uniqueness: true
 end
