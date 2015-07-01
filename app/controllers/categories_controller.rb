5c0c01f126e11f4e58df9ec5836cf4325051c365
Clayton Perszyk <clayperszyk@gmail.com>
2015-06-30 19:54:09 -0600

    add show and index actions to CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
 	def index
+		@categories = Category.order(:name)
 	end
 
 	def show
+		@category = Category.find(params[:id])
+		@articles = @category.articles.order(created_at: :desc)
 	end
 end
